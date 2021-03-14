17e591c44ba09c314890c376db0c1c2e4bd74995
mocavada <marc@awesomewebsite.ca>
2021-03-13 20:10:56 -0500

    first01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventAppComponent } from './event-app/event-app.component';
+import { EventListComponent } from './event-app/event-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    EventAppComponent,
+    EventListComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/event-app/event-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-app',
+  templateUrl: './event-app.component.html',
+  styleUrls: ['./event-app.component.css']
+})
+export class EventAppComponent  {
+
+}
